534ce8abec4c9fcf581c23929ccaf69730e00852
root <root@Ubuntu16.IndSoft>
2017-04-19 03:34:20 +0530

    gas added

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "2110" // Match any network id
+      network_id: "2110", // Match any network id
+      gas : 1500000
     }
   }
 };
